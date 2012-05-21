c8c2b43e46bd0a56297b2ea811a98b539e345234
Jamie White <jamie@jgwhite.co.uk>
2012-05-21 15:19:29 +0100

    v1.1.0

--- microfiche.js
+++ microfiche.js
@@ -1,4 +1,4 @@
-// # Microfiche.js v1.0.0
+// # Microfiche.js v1.1.0
 //
 // ## Usage
 //
@@ -66,7 +66,7 @@
 
 window.Microfiche = function(options) { this.initialize(options); return this; };
 
-Microfiche.VERSION = '1.0.0';
+Microfiche.VERSION = '1.1.0';
 
 $.extend(Microfiche.prototype, {
 
